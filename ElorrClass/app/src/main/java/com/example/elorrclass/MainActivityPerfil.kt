3c9cbb3575e1e00e4956c59bcf1f2d1b56a716e7
Maiory1 <156131673+Maiory1@users.noreply.github.com>
2025-01-17 17:48:56 +0100

    Implementaciones Vistas hechas, Strings para Español y Euskera para el cambio de idioma.

--- ElorrClass/app/src/main/java/com/example/elorrclass/MainActivityPerfil.kt
+++ ElorrClass/app/src/main/java/com/example/elorrclass/MainActivityPerfil.kt
@@ -1,6 +1,8 @@
 package com.example.elorrclass
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +11,12 @@ class MainActivityPerfil : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_perfil)
+
+        findViewById<Button>(R.id.button_VolverPanel).setOnClickListener {
+            val intent = Intent(applicationContext, MainActivityPanel::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
